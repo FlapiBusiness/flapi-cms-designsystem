0589df83aceed6468d196e3c388665269d1aa224
Corentin Recanzone <corentindevpro@hotmail.com>
2024-12-14 15:35:27 +0100

    fix: fixed tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,10 @@ import type { Config } from 'tailwindcss'
 
 /** @type {import('tailwindcss').Config} */
 export default (<Partial<Config>>{
-  content: ['./src/runtime/**/*.{js,ts,vue}'],
+  content: [
+    './src/runtime/**/*.{js,ts,vue}',
+    './src-app/src-nuxt/components/**/*.{vue,js,ts}', // a viré des que le fix pour /runtime est dispo
+  ],
   theme: {
     extend: {},
   },
