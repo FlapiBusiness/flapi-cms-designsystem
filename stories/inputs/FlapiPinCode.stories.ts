a30d84e68e9b86505076ddc70574b3ff3365dbe7
Léo Guillaume <leo.guillaume@epitech.eu>
2024-12-15 21:53:28 +0100

    feat: flapi pin code

---
+++ stories/inputs/FlapiPinCode.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryFn } from '@storybook/vue3'
+import FlapiPinCode from '@/components/inputs/FlapiPinCode.vue'
+import type { FlapiPinCodeProps } from '@/components/inputs/FlapiPinCode.vue'
+
+type FlapiPinCodeArgs = FlapiPinCodeProps & {
+  onConfirm?: (code: string) => void
+}
+
+export default {
+  title: 'inputs/FlapiPinCode',
+  component: FlapiPinCode,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/JukS6d8VqZfcuGHzUQixFg/cms-maquette-(FlapiCms)?node-id=456-3255&t=vY5X9uhmiQklBaE2-4',
+    },
+  },
+  argTypes: {
+    length: {
+      control: { type: 'number', min: 4, max: 12 },
+      description: 'The number of input fields for the pin code',
+      table: {
+        defaultValue: { summary: '6' },
+      },
+    },
+    onConfirm: {
+      action: 'confirm',
+      description: 'Triggered when the full pin code is entered',
+    },
+  },
+} as Meta<typeof FlapiPinCode>
+
+export const Playground: StoryFn<FlapiPinCodeArgs> = (args: FlapiPinCodeArgs) => ({
+  components: { FlapiPinCode },
+  setup() {
+    /**
+     * Handles the confirmation event.
+     * @param {string} code - The confirmed pin code
+     */
+    const handleConfirm: (code: string) => void = (code: string): void => {
+      console.log('Pin Code Entered:', code)
+      args.onConfirm?.(code)
+    }
+
+    return { args, handleConfirm }
+  },
+  template: `
+    <div class="w-64 mx-auto">
+      <FlapiPinCode v-bind="args" @confirm="handleConfirm" />
+    </div>
+  `,
+})
+
+Playground.args = {
+  length: 6,
+}
